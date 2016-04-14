fbe1ebac63d3428d8e772a10da34d795350e3b4b
Thomas Bastiani <thom@codehawks.eu>
2016-04-14 09:12:32 +0100

    Run pep8 to make my python better

--- restoc_gen.py
+++ restoc_gen.py
@@ -13,11 +13,11 @@
 #    * Neither the name of the organization nor the
 #      names of its contributors may be used to endorse or promote products
 #       derived from this software without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
 # DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 # (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 # LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -25,22 +25,24 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import sys
-import json
-import os
+import sys      # argv
+import json     # config file
+import os       # stat
+import hashlib  # sha1
+
 
 def printUsage():
-	print("Usage:")
-	print("\t./bintoc.py <resource_config.json> <resources_gen.c>")
-	exit(-1)
+    print("Usage:")
+    print("\t./bintoc.py <resource_config.json> <resources_gen.c>")
+    exit(-1)
 
 # Validate arguments
 if len(sys.argv) != 3:
-	printUsage()
+    printUsage()
 
 # Read JSON
 with open(sys.argv[1], "r") as jsonFile:
-	jsonStr = jsonFile.read()
+    jsonStr = jsonFile.read()
 resourceDesc = json.loads(jsonStr)
 
 # Generate *.c file
@@ -54,46 +56,46 @@ outputFile.write(headerStr)
 # Write content
 resourceIndex = 0
 for key in resourceDesc:
-	data = open(resourceDesc[key], "rb").read()
-        flen = os.stat(resourceDesc[key]).st_size
+    data = open(resourceDesc[key], "rb").read()
+    flen = os.stat(resourceDesc[key]).st_size
 
-        h = hashlib.sha1()
-        h.update(key)
-        sha1 = h.digest()
+    h = hashlib.sha1()
+    h.update(key)
+    sha1 = h.digest()
 
-	# Write data
-        outputFile.write(
-                    "static const uint8_t data_field_{}[{}] = {{".format(
-                    resourceIndex, max(flen, h.digest_size)))
+    # Write data
+    outputFile.write(
+        "static const uint8_t data_field_{}[{}] = {{".format(
+            resourceIndex, max(flen, h.digest_size)))
 
-	for i in range(0, len(sha1)):
-		outputFile.write(hex(ord(sha1[i])))
-		outputFile.write(",")
+    for i in range(0, len(sha1)):
+        outputFile.write(hex(ord(sha1[i])))
+        outputFile.write(",")
 
-	outputFile.write("};\n")
+    outputFile.write("};\n")
 
-	# Write struct
-	outputFile.write("resource_t resource_")
-	outputFile.write(str(resourceIndex))
-	outputFile.write(" = {\"")
-	outputFile.write(key)
-	outputFile.write("\",")
-	outputFile.write(str(flen))
-	outputFile.write(",data_field_")
-	outputFile.write(str(resourceIndex))
-	outputFile.write(",};\n")
-	
-	resourceIndex = resourceIndex + 1
+    # Write struct
+    outputFile.write("resource_t resource_")
+    outputFile.write(str(resourceIndex))
+    outputFile.write(" = {\"")
+    outputFile.write(key)
+    outputFile.write("\",")
+    outputFile.write(str(flen))
+    outputFile.write(",data_field_")
+    outputFile.write(str(resourceIndex))
+    outputFile.write(",};\n")
+
+    resourceIndex = resourceIndex + 1
 
 # Write global array
 outputFile.write("const resource_t *__named_resources_table[] = {")
 resourceIndex = 0
 for key in resourceDesc:
-	outputFile.write("(const resource_t *) (&resource_")
-	outputFile.write(str(resourceIndex))
-	outputFile.write("),")
+    outputFile.write("(const resource_t *) (&resource_")
+    outputFile.write(str(resourceIndex))
+    outputFile.write("),")
 
-	resourceIndex = resourceIndex + 1
+    resourceIndex = resourceIndex + 1
 
 outputFile.write("};\n")
 outputFile.write("const unsigned __named_resources_count = ")
